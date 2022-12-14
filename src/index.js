c90f01354afa5ca1058b5b8352d3779e7138dd80
Peterson <petersonpaganelli@outlook.com>
2022-12-14 16:42:04 -0300

    Requisito 3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const readFile = require('./middlewares/readFile');
+const { validateEmail, validatePassword } = require('./middlewares/validateFields');
 
 const app = express();
 app.use(express.json());
+
+const rand = () => Math.random().toString(32).substr(2, 8);
+
+const token = () => rand() + rand();
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -10,7 +15,7 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
-});
+}); 
 
 app.get('/talker', async (req, res) => {
   const talker = await readFile();
@@ -25,6 +30,11 @@ app.get('/talker/:id', async (req, res) => {
       return res.status(200).json(findTalker);
     } 
       res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
+ 
+app.post('/login', validateEmail, validatePassword, async (req, res) => {
+  res.status(200)
+  .json({ token: token() }); 
 });
 
 app.listen(PORT, () => {
