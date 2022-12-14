d89a70a6be83ffb3b47af96a0ec9f7e168222a37
Peterson <petersonpaganelli@outlook.com>
2022-12-14 15:28:18 -0300

    Requisito 2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-const readFile = require('./middlewares/readFile')
 const express = require('express');
+const readFile = require('./middlewares/readFile');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
-
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -15,8 +14,18 @@ app.get('/', (_request, response) => {
 
 app.get('/talker', async (req, res) => {
   const talker = await readFile();
-  res.status(200).json(talker)
-})
+  res.status(200).json(talker);
+});
+
+app.get('/talker/:id', async (req, res) => {
+    const { id } = req.params;
+    const talker = await readFile();  
+    const findTalker = talker.find((tal) => Number(tal.id) === Number(id));
+    if (findTalker) {
+      return res.status(200).json(findTalker);
+    } 
+      res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
 
 app.listen(PORT, () => {
   console.log('Online');
